6471c914067c7e446f857208e20173b3b10aafff
Bhanuprakash-1995 <bhanupadhu1995@gmail.com>
2024-01-12 19:39:01 +0530

    sg-commit

--- terraform-roboshop/02-sg/main.tf
+++ terraform-roboshop/02-sg/main.tf
@@ -1,9 +1,9 @@
 module "mongodb" {
-  source           = "../../terraform-aws-security-groups"
-  project_name     = var.project_name
-  environment      = var.environment
-  vpc_id           = data.aws_ssm_parameter.vpc_id.value
-  sg_ingress_rules = var.mongodb_sg_ingress_rules
-  sg_name          = "mongodb"
-  sg_description   = "SG for Mongodb"
+  source       = "../../terraform-aws-security-groups"
+  project_name = var.project_name
+  environment  = var.environment
+  vpc_id       = data.aws_ssm_parameter.vpc_id.value
+  # sg_ingress_rules = var.mongodb_sg_ingress_rules
+  sg_name        = "mongodb"
+  sg_description = "SG for Mongodb"
 }
